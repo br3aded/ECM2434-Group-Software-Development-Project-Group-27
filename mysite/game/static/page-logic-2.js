e7d965ff49d47645318e329430e96cb8f04f5664
MatthewWood-Github <matthewwood2002@gmail.com>
2023-02-17 14:06:34 +0000

    UI updates

--- mysite/game/static/page-logic-2.js
+++ mysite/game/static/page-logic-2.js
@@ -12,3 +12,15 @@ function setPageTitle() {
 }
 
 setPageTitle();
+
+function dim(id) {
+    let elem = document.getElementById(id);
+    elem.classList.add("button-animation");
+    elem.style.filter = ("brightness(0.5)");
+}
+
+function undim(id) {
+    let elem = document.getElementById(id);
+    elem.classList.remove("button-animation");
+    elem.style.filter = ("brightness(1)");
+}
