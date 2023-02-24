96ffc56c7f4536e1da94f02d649b1391128bea53
br3aded <kelseyRholdgate@icloud.com>
2023-02-24 13:04:32 +0000

    Added lobby view design
    Added a lobby view design template for how lobby view should work

--- mysite/game/views.py
+++ mysite/game/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render , redirect
+from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from django.urls import reverse
@@ -6,10 +6,7 @@ from django.template import loader
 from .models import Game 
 from django.contrib.auth.models import User
 from datetime import datetime
-from django.conf import settings
 from django.contrib.auth.decorators import login_required
-
-
 
 LOGGED_IN = True
 
@@ -25,7 +22,6 @@ def add_lobby(request):
     code = request.POST['lobby code']
     player = request.POST['num of players']
     rounds = request.POST['num of rounds']
-    game_master = request.user
     game = Game(start_datetime = datetime.now(),end_date = '', max_points = '', hosting_group = '') #will change when database is added
     game.save()
     #add code here for creating a new lobby item in database when implemented
@@ -35,8 +31,58 @@ def add_lobby(request):
 #generic lobby page
 #this will change when lobby implemented
 @login_required(login_url='/login/')
-def lobby_view(request):
+def lobby_view(request,user_id,game_code):
+    '''
+    rough outline of what the lobby should look like
+
+    lobby = get game_code lobby
+
+    if lobby state == 1:
+        return render waiting for players to join html
+    if lobby state == 2:
+        if user_id == lobby get game_master:
+            return render set tasks html
+        else:
+            return render waiting for task html
+    if lobby state == 3:
+        if user_id == lobby get game_master:
+            return render waiting for response html (possibly display active number of responses)
+        else:
+            return render response to task html
+    if lobby state == 4:
+        if user_id == lobby get game_master:
+            return render for ranking tasks html
+        else:
+            return render waiting for ranking html
+        return render template 4
+    if lobby state == 5:
+        return render results html (possibly add a button that will move state once all players have clicked it)
+    if lobby state == 6:
+        if lobby current round == total rounds:
+            return render display final results (possibly add a button here to delete lobby once all players have clicked it)
+        else:
+            lobby current round += 1
+            lobby current state = 2
+            return render lobby view
+
+    '''
+
     return render(request,"game/gamelobby.html")
+
+'''
+def set_task():
+    code for setting task here
+
+def response_task():
+    code for responding to task here
+
+def rank_tasks():
+    code for ranking tasks here
+
+'''
+
+
+
 
 '''
 def members(request):
@@ -47,12 +93,11 @@ def members(request):
 # /game url
 @login_required(login_url='/login/')
 def members(request):
-    if not request.user.is_authenticated:
-        return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
     template = loader.get_template('game/join_lobby.html')
     return HttpResponse(template.render())
 
-    
+
+@login_required(login_url='/login/')    
 def get_lobby_code(request):
     if request.method == "POST":
         code = request.POST["enter-code"]
