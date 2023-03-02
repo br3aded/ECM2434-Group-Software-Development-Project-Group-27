6c726aad1ad58d207fe6ebe54293719714255002
kelsey holdgate <62016085+br3aded@users.noreply.github.com>
2023-03-02 11:36:23 +0000

    Update urls.py

--- mysite/game/urls.py
+++ mysite/game/urls.py
@@ -2,16 +2,15 @@ from django.urls import path
 
 from . import views
 
-app_name = 'todo'
+app_name = 'game'
 urlpatterns = [
     path('create_lobby', views.create_lobby, name='create_lobby'),
     path('add_lobby',views.add_lobby ,name='add_lobby'),
     path('check_code', views.check_code, name='check_code'),
-    path('lobby',views.lobby_view,name='lobby_view'), #should include game code in lobby name
-    path('setting_task',views.set_task_view,name='setting_task'),
-    path('respond_task',views.response_task_view,name='respond_task'),
-    path('join_lobby',views.lobby_view,name='join_lobby'),
+    path('lobby/<str:game_code>/',views.lobby_view,name='lobby_view'), #should include game code in lobby name
+    path('setting_task',views.lobby_view,name='setting_task'),
+    path('game/join_lobby/<str:game_code>/', views.join_lobby, name='join_lobby'),
     path('game/', views.members, name='game'),
-
+    path('player_lobbys', views.player_lobbys,name='player_lobbys'),
     path('', views.members, name='game'),
 ]
