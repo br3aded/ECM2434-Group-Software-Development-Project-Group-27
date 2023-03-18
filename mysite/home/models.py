0b5fd1da76fe5fef040f93bc6a1ce424dc269603
nickoc294 <no294@exeter.ac.uk>
2023-03-18 20:16:33 +0000

    Further database upgrades

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -7,4 +7,13 @@ class Group(models.Model):
     group_name = models.CharField(max_length=32)
     group_leader = models.ForeignKey(AppUser,on_delete=models.PROTECT)#change?
 
-    group_members = models.ManyToManyField(AppUser, related_name="group_members")
+    group_members = models.ManyToManyField(AppUser, through="GroupMembers", related_name="group_members")
+
+class GroupMembers(models.Model):
+    user_id = models.ForeignKey(AppUser, on_delete=models.CASCADE) 
+    group_id = models.ForeignKey(Group, on_delete=models.CASCADE) 
+    final_position = models.IntegerField(default=0)
+    points_earned = models.IntegerField(default=0) #keeps a tally of points over the whole game
+
+    class Meta:
+        unique_together = ("user_id", "group_id")
