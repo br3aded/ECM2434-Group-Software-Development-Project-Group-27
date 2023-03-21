7e98705d558ee6fcf8ddc32c6145ddb363239287
br3aded <kelseyRholdgate@icloud.com>
2023-03-21 17:55:16 +0000

    Create game and set task fixed
    -Create game and set task now work

--- mysite/game/urls.py
+++ mysite/game/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('get_game_data', views.get_game_data, name='get_game_data'),
     path('lobby/<str:game_code>',views.lobby_view,name='lobby_view'), #should include game code in lobby name
     path('setting_task/<str:game_code>',views.set_task_view,name='setting_task_view'),
+    path('set_task/<str:game_code>', views.set_task , name = 'set_task'),
     path('join_lobby', views.get_game_data, name='join_lobby'),
     path('submit_task/<str:game_code>',views.submit_task,name='submit_task'),
     path('take_picture/<str:game_code>',views.take_picture,name='take_picture'),
